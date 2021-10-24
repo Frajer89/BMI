da100ab5737d221e49734b9e9c1bb77250cb19a3
Ondřej Kryštof <Krystof.Ondrej@stud.vassboskovice.cz>
2021-10-24 15:21:56 +0200

    vypocet-bmi

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -6,9 +6,38 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Výpočet BMI");
+            while (true)
+            {
+                 double weight;
+                 double height;
+    
+                
+                vaha:
+                Console.WriteLine("Zadej váhu (kg)");
+                weight = double.Parse(Console.ReadLine());
+            if (weight >= 200)
+            {
+               Console.WriteLine("Jsi příliš těžký, zadej váhu znovu!");
+               goto vaha;
+            }
+               
+               vyska:
+                Console.WriteLine("Zadej výšku(m)");
+                height = double.Parse(Console.ReadLine());
+            if (height >= 3)
+            {
+                Console.WriteLine("Jsi příliš vysoký, zadej výšku znovu!");
+                   goto vyska;
 
+            }
+
+                var celkem = weight / (height * height);
+                Console.WriteLine($"Vaše BMI je {celkem} ");
+                Console.WriteLine();
+
+            }
+           
         }
     }
 }
